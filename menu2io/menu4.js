2c41d6e630fd8a2ce8f185eba70238a6f41e4819
Andre-L-Moraes <113194322+Andre-L-Moraes@users.noreply.github.com>
2024-10-02 18:40:50 -0300

    Novo Loguin com Aranha

--- menu2io/menu4.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".menu_link").click(function(e) {
-    e.preventDefault(); // Evita o comportamento padrão do link
-    $(".menu_move").toggleClass("clicado"); // Alterna a classe "clicado" na navegação
-  });
-});
